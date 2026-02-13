5ebb14a541163529a3f1df242dea1bd646e81b7f
지준삼 <jsji@daou.co.kr>
2026-02-13 15:20:56 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
 	  base: '/subtitle-remove/',
-      plugins: [react()],
+      plugins: [
+		react(),
+		tailwindcss()
+	  ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
